9347fa3479b214d6dbae50cdf3b48bfc2df9f9e0
Cihat Girgin <cihatgirgin1@gmail.com>
2021-11-07 23:42:54 +0300

    hash type cache operations

--- RedisExchangeApp.Web/Controllers/HashTypeController.cs
+++ RedisExchangeApp.Web/Controllers/HashTypeController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RedisExchangeApp.Web.Services;
+using StackExchange.Redis;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +10,34 @@ namespace RedisExchangeApp.Web.Controllers
 {
     public class HashTypeController : Controller
     {
+        private readonly RedisService _redisService;
+        private readonly IDatabase _db;
+        private string _listKey = "HashNames";
+
+        public HashTypeController(RedisService redisService)
+        {
+            _redisService = redisService;
+            _db = _redisService.GetDb(4);
+        }
         public IActionResult Index()
         {
-            return View();
+            if (_db.KeyExists(_listKey))
+            {
+                Dictionary<string,string> nameList = new();
+                _db.HashGetAll(_listKey).ToList().ForEach(name =>
+                {
+                    nameList.Add(name.Name.ToString(),name.Value.ToString());
+                });
+                return View(nameList);
+            }
+            return View(new Dictionary<string,string>());
+        }
+        [HttpPost]
+        public IActionResult Add(string key,string value)
+        {
+            _db.KeyExpire(_listKey, DateTime.Now.AddMinutes(1));
+            _db.HashSet(_listKey, key, value);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
