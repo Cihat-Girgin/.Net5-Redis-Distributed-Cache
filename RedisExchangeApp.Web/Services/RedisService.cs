9a2429b3a8640563e3d38269ecec3bfd066b1ac1
Cihat Girgin <cihatgirgin1@gmail.com>
2021-11-07 20:56:41 +0300

    Update RedisService.cs

--- RedisExchangeApp.Web/Services/RedisService.cs
+++ RedisExchangeApp.Web/Services/RedisService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using StackExchange.Redis;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +9,27 @@ namespace RedisExchangeApp.Web.Services
 {
     public class RedisService
     {
+        private readonly string _redisHost;
+        
+        private readonly string _redisPort;
+
+        private ConnectionMultiplexer _redis;
+
+        public IDatabase Db { get; set; }
+        public RedisService(IConfiguration configuration)
+        {
+            _redisHost = configuration["Redis:Host"];
+            _redisPort = configuration["Redis:Port"];
+        }
+
+        public void Connect()
+        {
+            var config = $"{_redisHost}:{_redisPort}";
+            _redis = ConnectionMultiplexer.Connect(config);
+        }
+        public IDatabase GetDb(int db)
+        {
+            return _redis.GetDatabase(db);
+        }
     }
 }
