4642c3ba3390db4a0a1ad724accbff40e6e977a1
Cihat Girgin <cihatgirgin1@gmail.com>
2021-11-07 22:45:22 +0300

    List type cache operations

--- RedisExchangeApp.Web/Controllers/ListTypeController.cs
+++ RedisExchangeApp.Web/Controllers/ListTypeController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RedisExchangeApp.Web.Services;
+using StackExchange.Redis;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +10,38 @@ namespace RedisExchangeApp.Web.Controllers
 {
     public class ListTypeController : Controller
     {
+
+        private readonly RedisService _redisService;
+        private readonly IDatabase _db;
+        private string _listKey = "Names";
+
+        public ListTypeController(RedisService redisService)
+        {
+            _redisService = redisService;
+            _db = _redisService.GetDb(1);
+        }
         public IActionResult Index()
         {
-            return View();
+            if (_db.KeyExists(_listKey))
+            {
+                List<string> namesList = new();
+                _db.ListRange(_listKey).ToList().ForEach(name =>
+                {
+                    namesList.Add(name.ToString());
+                });
+                return View(namesList);
+            }
+
+            return View(new List<string>());
         }
+
+        [HttpPost]
+        public IActionResult Add(string name)
+        {
+            _db.ListRightPush(_listKey, name);
+            return RedirectToAction(nameof(Index));
+        }
+        
     }
+
 }
