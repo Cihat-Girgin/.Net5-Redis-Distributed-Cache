74af70cf1a707731bf3e6233a1802557358215a3
Cihat Girgin <cihatgirgin1@gmail.com>
2021-11-07 20:37:00 +0300

    initialize project

---
+++ RedisExchangeApp.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RedisExchangeApp.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
