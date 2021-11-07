0bcaa9f2ed63d3a5f44d7db166db8b8c0cf72a5a
Cihat Girgin <cihatgirgin1@gmail.com>
2021-11-07 21:44:11 +0300

    String type cache operations

--- RedisExchangeApp.Web/Controllers/StringTypeController.cs
+++ RedisExchangeApp.Web/Controllers/StringTypeController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RedisExchangeApp.Web.Services;
+using StackExchange.Redis;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,8 +10,26 @@ namespace RedisExchangeApp.Web.Controllers
 {
     public class StringTypeController : Controller
     {
+        private readonly RedisService _redisService;
+        private readonly IDatabase _db;
+
+        public StringTypeController(RedisService redisService)
+        {
+            _redisService = redisService;
+            _db = _redisService.GetDb(0);
+        }
         public IActionResult Index()
         {
+            _db.StringSet("Name", "Example User");
+            return View();
+        }
+        public IActionResult Show()
+        {
+            var value = _db.StringGet("Name");
+            if (value.HasValue)
+            {
+                ViewBag.Value = value.ToString();
+            }
             return View();
         }
     }
