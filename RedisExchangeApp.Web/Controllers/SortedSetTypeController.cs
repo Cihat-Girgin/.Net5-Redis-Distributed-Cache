ab462daea20247e2eb63880ad69ac39e4cbe70b9
Cihat Girgin <cihatgirgin1@gmail.com>
2021-11-07 23:25:39 +0300

    SortedSet type cache operations

---
+++ RedisExchangeApp.Web/Controllers/SortedSetTypeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RedisExchangeApp.Web.Services;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedisExchangeApp.Web.Controllers
+{
+    public class SortedSetTypeController : Controller
+    {
+        private readonly RedisService _redisService;
+        private readonly IDatabase _db;
+        private string _listKey = "SortedSetNames";
+
+        public SortedSetTypeController(RedisService redisService)
+        {
+            _redisService = redisService;
+            _db = _redisService.GetDb(3);
+        }
+        public IActionResult Index()
+        {
+            if (_db.KeyExists(_listKey))
+            {
+                SortedSet<string> nameList = new();
+                _db.SortedSetScan(_listKey).ToList().ForEach(name =>
+                {
+                    nameList.Add(name.ToString());
+                });
+                return View(nameList);
+            }
+            return View(new SortedSet<string>());
+        }
+        public IActionResult Add(string name,int score)
+        {
+            _db.KeyExpire(_listKey, DateTime.Now.AddMinutes(1));
+            _db.SortedSetAdd(_listKey, name, score);
+            return RedirectToAction(nameof(Index)); 
+        }
+    }
+}
