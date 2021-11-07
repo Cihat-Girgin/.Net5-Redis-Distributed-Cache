6122eb45eb76175d52c15066393d78c3e50f4a8b
Cihat Girgin <cihatgirgin1@gmail.com>
2021-11-07 22:02:24 +0300

    Create ListTypeController.cs

---
+++ RedisExchangeApp.Web/Controllers/ListTypeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedisExchangeApp.Web.Controllers
+{
+    public class ListTypeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
