84a88453c718e135fb47a02c3dd75fb872c4e61a
Cihat Girgin <cihatgirgin1@gmail.com>
2021-11-07 23:08:26 +0300

    Set type cache operations

--- RedisExchangeApp.Web/Controllers/SetTypeController.cs
+++ RedisExchangeApp.Web/Controllers/SetTypeController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RedisExchangeApp.Web.Services;
+using StackExchange.Redis;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +10,40 @@ namespace RedisExchangeApp.Web.Controllers
 {
     public class SetTypeController : Controller
     {
+        private readonly RedisService _redisService;
+        private readonly IDatabase _db;
+        private string _listKey = "HashNames";
+
+
+        public SetTypeController(RedisService redisService)
+        {
+            _redisService = redisService;
+            _db = _redisService.GetDb(2);
+        }
         public IActionResult Index()
         {
-            return View();
+            if (_db.KeyExists(_listKey))
+            {
+                HashSet<string> namesList = new();
+                _db.SetMembers(_listKey).ToList().ForEach(name =>
+                {
+                    namesList.Add(name.ToString());
+                });
+                return View(namesList);
+            }
+            return View(new HashSet<string>());
+        }
+        [HttpPost]
+        public IActionResult Add(string name)
+        {
+            if (!_db.KeyExists(_listKey))
+            {
+                _db.KeyExpire(_listKey, DateTime.Now.AddMinutes(5));
+            }
+
+            _db.SetAdd(_listKey, name);
+            
+            return RedirectToAction(nameof(Index));
         }
     }
 }
